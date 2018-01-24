8c00509d07fcb24b93fdba17d59a31decc7e7e1d
Jesse Luoto <jesse.luoto@gmail.com>
2018-01-24 09:57:32 +0200

    Add prettier

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { Buffer } = require('buffer')
 
-function writeTile(colors = Array(8*8).fill(0)) {
+function writeTile(colors = Array(8 * 8).fill(0)) {
   const buffer = Buffer.allocUnsafe(16)
 
   for (let row = 0; row < 8; row++) {
     const index = row * 8
-    const highBits = getUintValue(colors.slice(index, index+8).map(v => v & 2 ? 1 : 0))
+    const highBits = getUintValue(
+      colors.slice(index, index + 8).map(v => (v & 2 ? 1 : 0))
+    )
     buffer.writeUInt8(highBits, row)
   }
 
   for (let row = 0; row < 8; row++) {
     const index = row * 8
-    const lowBits = getUintValue(colors.slice(index, index+8).map(v => v & 1))
+    const lowBits = getUintValue(colors.slice(index, index + 8).map(v => v & 1))
     buffer.writeUInt8(lowBits, row + 8)
   }
 
@@ -20,8 +22,8 @@ function writeTile(colors = Array(8*8).fill(0)) {
 
 function getUintValue(values) {
   let result = 0
-  for(let i = 0; i < values.length; i++) {
-    result |= (values[i]) << (values.length - 1 - i)
+  for (let i = 0; i < values.length; i++) {
+    result |= values[i] << (values.length - 1 - i)
   }
   return result
 }
--- index.spec.js
+++ index.spec.js
@@ -7,47 +7,54 @@ describe('writeTile', () => {
   })
 
   it('should return a buffer with 16 zeros', () => {
-    expect(writeTile().toString('hex')).to.eql('00000000000000000000000000000000')
+    const zeros = '00000000000000000000000000000000'
+    expect(writeTile().toString('hex')).to.eql(zeros)
   })
 
   it('should return a buffer filled with true bits if passed an array of 3s', () => {
-    const input = Array(8*8).fill(3)
-    expect(writeTile(input).toString('hex')).to.eql('ffffffffffffffffffffffffffffffff')
+    const ones = 'ffffffffffffffffffffffffffffffff'
+    const input = Array(8 * 8).fill(3)
+    expect(writeTile(input).toString('hex')).to.eql(ones)
   })
 
   it('should return a correct buffer for 1', () => {
-    const input = Array(8*8).fill(0)
+    const result = '00000000000000008000000000000000'
+    const input = Array(8 * 8).fill(0)
     input[0] = 1
 
-    expect(writeTile(input).toString('hex')).to.eql('00000000000000008000000000000000')
+    expect(writeTile(input).toString('hex')).to.eql(result)
   })
 
   it('should return a correct buffer for 2', () => {
-    const input = Array(8*8).fill(0)
+    const result = '80000000000000000000000000000000'
+    const input = Array(8 * 8).fill(0)
     input[0] = 2
 
-    expect(writeTile(input).toString('hex')).to.eql('80000000000000000000000000000000')
+    expect(writeTile(input).toString('hex')).to.eql(result)
   })
 
   it('should return a correct buffer for 3', () => {
-    const input = Array(8*8).fill(0)
+    const result = '80000000000000008000000000000000'
+    const input = Array(8 * 8).fill(0)
     input[0] = 3
 
-    expect(writeTile(input).toString('hex')).to.eql('80000000000000008000000000000000')
+    expect(writeTile(input).toString('hex')).to.eql(result)
   })
 
   it('should return a correct buffer for two ones', () => {
-    const input = Array(8*8).fill(0)
+    const result = '00000000000000008080000000000000'
+    const input = Array(8 * 8).fill(0)
     input[0] = 1
     input[8] = 1
 
-    expect(writeTile(input).toString('hex')).to.eql('00000000000000008080000000000000')
+    expect(writeTile(input).toString('hex')).to.eql(result)
   })
 
   it('should return a correct buffer for full ones', () => {
-    const input = Array(8*8).fill(1)
+    const result = '0000000000000000ffffffffffffffff'
+    const input = Array(8 * 8).fill(1)
 
-    expect(writeTile(input).toString('hex')).to.eql('0000000000000000ffffffffffffffff')
+    expect(writeTile(input).toString('hex')).to.eql(result)
   })
 })
 
@@ -65,16 +72,16 @@ describe('getUintValue', () => {
   })
 
   it('should return 3 when passed (1,1)', () => {
-    expect(getUintValue([1,1])).to.eql(3)
+    expect(getUintValue([1, 1])).to.eql(3)
   })
 
   it('should return complex binary value', () => {
-    const input = [1,1,0,1,1,0,0,1]
+    const input = [1, 1, 0, 1, 1, 0, 0, 1]
     expect(getUintValue(input)).to.eql(parseInt(input.join(''), 2))
   })
 
   it('should return correct value when only one 1 is passed', () => {
-    const input = [1,0,0,0,0,0,0,0]
+    const input = [1, 0, 0, 0, 0, 0, 0, 0]
     expect(getUintValue(input)).to.eql(128)
   })
 })
